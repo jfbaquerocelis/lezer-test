fa1ae357e3297c21027cc3d7f45b6ec4fc711cd6
John Baquero Celis <jfbaquerocelis@gmail.com>
2022-09-05 11:03:04 -0500

    add: hooks for resolving query and add types

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,57 @@
-import { useEffect, useState } from 'react';
-import { github } from './config';
-import { userQuery } from './queries/user';
+import { useQuery } from '@apollo/client';
+import { GET_REPOSITORIES } from './queries/user';
 
 interface Repository {
   id: string;
   name: string;
-  description: string;
+  description: string | null;
   url: string;
+  licenseInfo: {
+    spdxId: string;
+  } | null;
+}
+interface User {
+  id: string;
+  name: string;
+  avatarUrl: string;
+  repositories: {
+    edges: Edges[];
+    total: number;
+  },
+}
+interface Edges {
+  cursor: string;
+  node: Repository;
+}
+interface PageInfo {
+  startCursor?: string;
+  endCursor?: string;
+  hasNextPage?: boolean;
+  hasPreviousPage?: boolean;
+}
+
+interface RepositoriesData {
+  user: User;
+  pageInfo: PageInfo;
+}
+
+interface RepositoriesVars {
+  username: string;
+  cursor?: string;
 }
 
 function App() {
-  const [username, setUsername] = useState<string>("");
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const { loading, error, data } = useQuery<RepositoriesData, RepositoriesVars>(GET_REPOSITORIES, { variables: { username: 'jfbaquerocelis' } });
 
-  const getUser = async () => {
-    try {
-      const response = await fetch(github.baseURL, { method: "POST", headers: github.headers, body: JSON.stringify(userQuery) });
+  if (loading) return <>Loading...</>;
+  if (error) return <>Something went wrong!</>;
+  if (!data) return <>No data available</>;
 
-      if (response.ok) {
-        const { data: { viewer } } = await response.json();
-        setUsername(viewer.name);
-        setRepositories(viewer.repositories.nodes);
-      }
-      else throw new Error("Something went wrong");
-    } catch (error) {
-      console.error(error);
-      return null;
-    }
-  }
+  const repositories = data.user.repositories.edges.map((edge) => edge.node);
   
-  useEffect(() => {
-    getUser();
-  }, []);
-  
-
   return (
-    <div className="App container mt-5">
-      <h1 className='text-primary'><i className='bi bi-diagram-2-fill'></i> Repos</h1>
-      <p>Hey there {username}!</p>
+    <div className="container mt-5">
+      <h1 className='text-primary'><i className='bi bi-diagram-2-fill'></i> Repositories</h1>
       <ul>
         {repositories.map((repository) => <li key={repository.id}>{repository.name}</li>)}
       </ul>
