abb19a40b44ffe603f12a273eff1c42fe580b962
John Baquero Celis <jfbaquerocelis@gmail.com>
2022-09-05 11:02:10 -0500

    change: query repositories based on the current user seeked by username

--- src/queries/user.ts
+++ src/queries/user.ts
@@ -1,17 +1,36 @@
-export const userQuery = {
-  query: `
-    {
-      viewer {
-        name
-        repositories(first: 10) {
-          nodes {
+// https://docs.github.com/es/graphql/overview/explorer
+import { gql } from '@apollo/client';
+
+export const GET_REPOSITORIES = gql`  
+  query Repositories($cursor: String, $username: String!) {
+    user(login: $username) {
+      name
+      avatarUrl
+      repositories(
+        first: 5
+        after: $cursor
+        orderBy: {field: CREATED_AT, direction: DESC}
+      ) {
+        total: totalCount
+        edges {
+          cursor
+          node {
             id
             name
             description
             url
+            licenseInfo {
+              spdxId
+            }
           }
+        }
+        pageInfo {
+          startCursor
+          endCursor
+          hasNextPage
+          hasPreviousPage
         }
       }
     }
-  `
-};
+  }
+`;
