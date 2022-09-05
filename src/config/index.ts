b065277f3d2f3fa9b6797194bb0832c51f8e1656
John Baquero Celis <jfbaquerocelis@gmail.com>
2022-09-05 11:00:57 -0500

    add: apollo provider and cache configuration

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,5 @@
 export const github = {
     baseURL: "https://api.github.com/graphql",
-    username: "jfbaquerocelis",
     headers: {
         "Content-Type": "application/json",
         "Authorization": `bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+import { github } from './config';
 import App from './App';
 import './custom.scss';
+import { relayStylePagination } from '@apollo/client/utilities';
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        repositories: relayStylePagination()
+      }
+    }
+  }
+})
+
+const client = new ApolloClient({
+  uri: github.baseURL,
+  headers: github.headers,
+  cache,
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App /> 
+    </ApolloProvider>
   </React.StrictMode>
 );
